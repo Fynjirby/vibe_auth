db09951a05639488ac8cd3453301f8017e984be3
Egor <109822937+Fynjirby@users.noreply.github.com>
2025-04-09 18:54:15 +0300

    update

--- main.go
+++ main.go
@@ -3,18 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var first, second, third, fourth string
+	var answer string
 
 	fmt.Println("Enter password: ")
-	fmt.Scanln(&first)
-	if first == "password" { } else { return }
+	fmt.Scanln(&answer)
+	if answer == "password" { } else { return }
 	fmt.Println("Password is incorrect ")
-	fmt.Scanln(&second)
-	if second == "incorrect" { } else { return }
+	fmt.Scanln(&answer)
+	if answer == "incorrect" { } else { return }
 	fmt.Println("Try again. ")
-	fmt.Scanln(&third)
-	if third == "again" { } else { return }
+	fmt.Scanln(&answer)
+	if answer == "again" { } else { return }
 	fmt.Println("Please try again later")
-	fmt.Scanln(&fourth)
-	if fourth == "again later" { } else { return }
+	fmt.Scanln(&answer)
+	return
 }
